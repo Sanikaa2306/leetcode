091f47552b8c35adc9b20a3831c2758a3f19b355
Sanika Popat Hingalkar <sanuhingalkar2004@gmail.com>
2025-06-29 10:50:23 +0530

    Time: 473 ms (53.13%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,5 @@
+/* Write your T-SQL query statement below */
+select w1.id from Weather w1
+join Weather w2
+on datediff(day, w1.recordDate, w2.recordDate) = -1
+where w1.temperature > w2.temperature;
